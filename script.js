3c2236653b7539213fc690e992f9126b95181a42
TatyanaSchmidt <tanu_ru@yahoo.com>
2022-11-18 16:13:56 +0300

    for debagging

--- script.js
+++ script.js
@@ -1,70 +1,65 @@
 let errors = [];
-const firstName = document.querySelector('#first-name');
-console.log(firstName);
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const passwordConfirm = document.querySelector('#password-comfirm');
 
 function checkValidity(input){
     let validity = input.validity;
+
     if (validity.valueMissing) {
         errors.push('Поле ' + input.placeholder + ' не заполнено');
     }
 } 
 
-function checkAll() {
-    const val = firstName.validity.valueMissing;
-    const mean = firstName.value;
-    console.log(val);
-    console.log(mean);
+function validateEmail (email){
+    let mailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+    if (email.value.match(mailFormat)){
+        // return true;
+    }
+    else {
+        errors.push('Ваш адрес электронной почты введен неверно');
+        // return false;
+    }
+}
 
+function validatePassword (password){
+    let passwordFormat = /^[a-z0-9_-]{8,16}$/;
+
+    if (password.value.match(passwordFormat)){
+        // return true;
+    }
+    else {
+        errors.push('Ваш пароль некорректен');
+        // return false;
+    }
+}
+
+function validatePasswordConfirm (passwordConfirm){
+    if (passwordConfirm.value === password.value){
+        // return true;
+    }
+    else {
+        errors.push('Ваш пароли не совпали');
+        // return false;
+    }
+}
+
+const clearInput = (inputs) => {
+    inputs.value = '';
+}
+
+function checkAll() {
     errors = [];
     let inputs = document.querySelectorAll('input');
+
     for (let input of inputs) {
         checkValidity(input);
     }
+
+    validateEmail (email);
+    validatePassword (password);
+    validatePasswordConfirm (passwordConfirm);
+
     document.getElementById('errorsInfo').innerHTML = errors.join('. <br>');
+    clearInput(inputs);
 }
-
-
-
-
-
-
-// function validateEmail (email){
-//     let mailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
-//     if (email.value.match(mailFormat)){
-//         return true;
-//     }
-//     else {
-//         errors.push('Ваш адрес электронной почты введен неверно');
-//         return false;
-//     }
-// }
-
-// valueMissing
-// badInput
-// customError
-// patternMismatch
-// rangeOverflow
-// rangeUnderflow
-// stepMismatch
-// tooLong
-// tooShort
-// typeMismatch
-// valid
-
-// if (validity.rangeOverflow) {
-//     let max = getAttributeValue(input,'max');
-//     errors.push('Максимальное значение не может быть больше чем ' + max);
-// }
-// if (validity.rangeUnderflow) {
-//     let min = getAttributeValue(input,'min');
-//     errors.push('Минимальное значение не может быть больше чем ' + min);
-// }
-
-
-// const firstName = document.querySelector('#first-name');
-// const lastName = document.querySelector('#last-name');
-// const login = document.querySelector('#login');
-// const password = document.querySelector('#password');
-// const passwordConfirm = document.querySelector('#password-comfirm');
-// const email = document.querySelector('#email');
-// const submit = document.querySelector('#btn-submit');
